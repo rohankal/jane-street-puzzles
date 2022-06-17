d4bac0ccf1a9bd4108ec190768a88446b8d2eb2d
Rohan Kalra <rohankal@usc.edu>
2022-06-16 23:28:08 -0400

    need to implement isSafe()

--- june-2022/main.cpp
+++ june-2022/main.cpp
@@ -4,7 +4,11 @@
 #include <set>
 #include <vector>
 #include <map>
+#include "prettyprint.hpp"
+
 using namespace std;
+
+#define N 5
 
 struct PuzzleCell
 {
@@ -18,7 +22,9 @@ struct PuzzleCell
 
 vector<vector<PuzzleCell>> puzzle;
 map<char, int> regionSizes{
-    {'a', 5}, {'b', 5}, {'c', 5}, {'d', 1}, {'e', 1}, {'f', 4}, {'g', 3}};
+    {'a', 5}, {'b', 5}, {'c', 5}, {'d', 1}, {'e', 1}, {'f', 4}, {'g', 3}, {'h', 1}};
+map<char, set<int>> regionUsed{
+    {'a', {}}, {'b', {}}, {'c', {}}, {'d', {}}, {'e', {}}, {'f', {}}, {'g', {}}, {'h', {}}};
 
 void populatePuzzle()
 {
@@ -68,12 +74,87 @@ void displayPuzzle()
 
 set<int> getPossibleValuesOfCell(int row, int col)
 {
-    return puzzle[row][col].possibleValues;
+    set<int> toRet = puzzle[row][col].possibleValues;
+    cout << toRet << endl;
+    return toRet;
+}
+
+int calculateAnswer()
+{
+    int toRet = 0;
+    int rowProduct;
+    for (int i = 0; i < puzzle.size(); i++)
+    {
+        rowProduct = 1;
+        for (int j = 0; j < puzzle[0].size(); j++)
+        {
+            rowProduct *= puzzle[i][j].assignedValue;
+        }
+        toRet += rowProduct;
+    }
+    return toRet;
+}
+
+bool isSafe(int row, int col, int num)
+{
+    set<int> used = regionUsed[puzzle[row][col].region];
+    cout << "Used Set: " << used << endl;
+    if (used.find(num) != used.end())
+    {
+        return false;
+    }
+    return true;
+}
+
+bool solvePuzzle(int row, int col)
+{
+    if (row == N - 1 && col == N)
+        return true;
+
+    if (col == N)
+    {
+        row++;
+        col = 0;
+    }
+
+    if (puzzle[row][col].assignedValue != 0)
+        return solvePuzzle(row, col + 1);
+
+    set<int> possibleNumbers = getPossibleValuesOfCell(row, col);
+
+    for (auto num : possibleNumbers)
+    {
+        cout << num << " " << endl;
+        if (isSafe(row, col, num))
+        {
+            puzzle[row][col].assignedValue = num;
+            regionUsed[puzzle[row][col].region].insert(num);
+            displayPuzzle();
+            cout << endl;
+            if (solvePuzzle(row, col + 1))
+                return true;
+        }
+        else
+        {
+            regionUsed[puzzle[row][col].region].erase(num);
+            puzzle[row][col].assignedValue = 0;
+        }
+    }
+    return false;
 }
 
 int main()
 {
     populatePuzzle();
-    displayPuzzle();
+    if (solvePuzzle(0, 0))
+    {
+
+        displayPuzzle();
+        cout << calculateAnswer() << endl;
+    }
+    else
+    {
+        cout << "There is no solution to this particular puzzle." << endl;
+    }
     return 0;
 }
